2149cbd4144e5371eeae62a4e73a89bc9de6a362
joseph0x4365732E <63211229+joseph0x4365732E@users.noreply.github.com>
2021-11-21 13:49:47 -0800

    Initial Commit

---
+++ Shared/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shared
+//
+//  Created by Joseph Cestone on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
